1dd4688a2bd4d9794c73a172fc86d2f0b1c33f11
MaDLuck5 <86706711+MaDLuck5@users.noreply.github.com>
2024-03-03 14:44:16 +0100

    added new model script for H2

---
+++ Workbooks/Chapter2/model.py
@@ -0,0 +1,192 @@
+import numpy
+
+
+class Perceptron:
+    def __init__(self, dim, bias=0, weights=None, learning_rate=0.01):
+        """
+
+        :param dim:
+        :param bias:
+        :param weights:
+        :param learning_rate:
+        """
+        self.dim = dim
+        self.bias = bias
+        self.learning_rate = learning_rate
+        if weights is None:
+            # bij geen opgegeven weights gelijk stellen aan 0 en de opgegeven dimensie
+            self.weights = [0] * dim
+        else:
+            # aantal weights gelijk gesteld aan de dimensie
+            assert len(weights) == dim, "Length of weights should match the dimensionality"
+            self.weights = weights
+
+    def predict(self, xs):
+        """
+
+        :param xs:
+        :return:
+        """
+        predictions = []
+        for instance in xs:
+            prediction = self.predict_instance(instance)
+            predictions.append(prediction)
+        return predictions
+
+    def predict_instance(self, instance):
+        """
+        :param instance:
+        :return:
+        """
+        prediction = self.bias
+        for i in range(len(instance)):
+            prediction += self.weights[i] * instance[i]
+        return numpy.sign(prediction)
+
+    def partial_fit(self, xs, ys):
+        """
+
+        :param self:
+        :param xs:
+        :param ys:
+        :return:
+        """
+        for x, y in zip(xs, ys):
+            prediction = self.predict_instance(x)
+            self.bias -= (prediction - y)
+            for i in range(len(x)):
+                self.weights[i] -= (prediction - y) * x[i]
+
+    def fit(self, xs, ys, *, epochs=0):
+        """
+
+        :param self:
+        :param xs:
+        :param ys:
+        :param epochs:
+        :return:
+        """
+        prev_weights = self.weights
+        prev_bias = self.bias
+        epoch = 0
+        while True:
+            self.partial_fit(xs, ys)
+            if prev_weights == self.weights and prev_bias == self.bias:
+                break  # geen veranderingen in de weights en bias dus stoppen
+            # updaten van weight en bias
+            prev_weights = self.weights.copy()
+            prev_bias = self.bias
+            epoch += 1
+            if epochs != 0 and epoch >= epochs:
+                break  # stop als het opgeven max aantal epochs behaald is
+
+    def __repr__(self):
+        text = f'Perceptron(dim={self.dim}, bias={self.bias}, weights={self.weights})'
+        return text
+
+
+class LinearRegression:
+    def __init__(self, dim, bias=0, weights=None, learning_rate=0.01):
+        self.dim = dim
+        self.bias = bias
+        self.learning_rate = learning_rate
+        if weights is None:
+            # bij geen opgegeven weights gelijk stellen aan 0 en de opgegeven dimensie
+            self.weights = [0] * dim
+        else:
+            # aantal weights gelijk gesteld aan de dimensie
+            assert len(weights) == dim, "Length of weights should match the dimensionality"
+            self.weights = weights
+
+    def predict(self, xs):
+        predictions = []
+        for instance in xs:
+            prediction = self.predict_instance(instance)
+            predictions.append(prediction)
+        return predictions
+
+    def predict_instance(self, instance):
+        prediction = self.bias
+        for i in range(len(instance)):
+            prediction += self.weights[i] * instance[i]
+        return prediction
+
+    def partial_fit(self, xs, ys):
+        for x, y in zip(xs, ys):
+            prediction = self.predict_instance(x)
+            self.bias -= self.learning_rate * (prediction - y)
+            for i in range(len(x)):
+                self.weights[i] -= self.learning_rate * (prediction - y) * x[i]
+
+    def fit(self, xs, ys, *, epochs=0):
+        prev_weights = self.weights
+        prev_bias = self.bias
+        epoch = 0
+        while True:
+            self.partial_fit(xs, ys)
+            if prev_weights == self.weights and prev_bias == self.bias:
+                break  # geen veranderingen in de weights en bias dus stoppen
+            # updaten van weight en bias
+            prev_weights = self.weights.copy()
+            prev_bias = self.bias
+            epoch += 1
+            if epochs != 0 and epoch >= epochs:
+                break  # stop als het opgeven max aantal epochs behaald is
+
+    def __repr__(self):
+        text = f'LinearRegression(dim={self.dim}, bias={self.bias}, weights={self.weights})'
+        return text
+
+
+class Neuron():
+
+    def __repr__(self):
+        text = f'Neuron(dim={self.dim}, activation={self.activation.__name__}, loss={self.loss.__name__})'
+        return text
+
+
+def linear(x):
+    """
+    Linear activation function
+    :param x:
+    :return: x
+    """
+    return x
+
+
+def sign(x):
+    """
+    Signum activation function
+    :param x:
+    :return: 1 for positive, -1 for negative numbers of X
+    """
+    return 1 if x > 0 else -1
+
+
+def mean_squared_error(y_true, y_pred):
+    """
+    Mean squared error, returns the mean squared error of the prediction
+    :param y_true:
+    :param y_pred:
+    :return:
+    """
+    return (y_pred - y_true) ** 2
+
+
+def mean_absolute_error(y_true, y_pred):
+    """
+    returns the mean absolute error of the prediction
+    :param y_true:
+    :param y_pred:
+    :return:
+    """
+    return abs(y_pred - y_true)
+
+
+def derivative(function, delta=1e-5):
+    def wrapper_derivative(x, *args):
+        return (function(x + delta, *args) - function(x - delta, *args)) / (2 * delta)
+
+    wrapper_derivative.__name__ = function.__name__ + "'"
+    wrapper_derivative.__qualname__ = function.__qualname__ + "'"
+    return wrapper_derivative
