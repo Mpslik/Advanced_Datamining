94417926edee434845573c62e3f6c695f338a29e
Mats Slik <86706711+MaDLuck5@users.noreply.github.com>
2024-03-05 23:17:58 +0100

    streamline predict function and partial_fit also adde comments

--- Workbooks/Chapter2/model.py
+++ Workbooks/Chapter2/model.py
@@ -5,6 +5,7 @@ import numpy as np
 def linear(x):
     """
     Linear activation function
+    returns x
     :param x:
     :return: x
     """
@@ -14,6 +15,8 @@ def linear(x):
 def sign(x):
     """
     Signum activation function
+    that return a value of -1 or 1 depending on the value of x
+    -1 for negative numbers and 1 for positive numbers
     :param x:
     :return: 1 for positive, -1 for negative numbers of X
     """
@@ -21,10 +24,23 @@ def sign(x):
 
 
 def tanh(x):
+    """
+    Tanh activation function
+    returns the tanh value of given  x
+    :param x:
+    :return:
+    """
     return math.tanh(x)
 
 
 def hinge(y_true, y_phred):
+    """
+    Hinge activation function
+    r
+    :param y_true:
+    :param y_phred:
+    :return:
+    """
     return max(1 - y_phred * y_true, 0)
 
 
@@ -49,6 +65,14 @@ def mean_absolute_error(y_true, y_pred):
 
 
 def derivative(function, delta=0.01):
+    """
+    Calculates the derivative of a function for the given value.
+    used for calculation of the slope at a given point.
+    :param function:
+    :param delta: the step size to use in derivative calculation
+    :return:
+    """
+
     def wrapper_derivative(x, *args):
         return (function(x + delta, *args) - function(x - delta, *args)) / (2 * delta)
 
@@ -58,13 +82,14 @@ def derivative(function, delta=0.01):
 
 
 class Perceptron:
+    """
+    A simple perceptron class
+    used for solving binary linear separable data
+    """
+
     def __init__(self, dim, bias=0, weights=None, learning_rate=0.01):
         """
 
-        :param dim:
-        :param bias:
-        :param weights:
-        :param learning_rate:
         """
         self.dim = dim
         self.bias = bias
@@ -79,9 +104,9 @@ class Perceptron:
 
     def predict(self, xs):
         """
-
-        :param xs:
-        :return:
+        Calculates the prediction of the given list of instance with the stored weights and bias
+        :param xs: a list of instances
+        :return: list of predictions
         """
         predictions = []
         for instance in xs:
@@ -90,21 +115,24 @@ class Perceptron:
         return predictions
 
     def predict_instance(self, instance):
+
         """
-        :param instance:
-        :return:
+        predicts the given instance with the stored weights and bias
+        :param instance: a single instance array
+        :return: the predicted value int
         """
         prediction = self.bias
         for i in range(len(instance)):
             prediction += self.weights[i] * instance[i]
-        return numpy.sign(prediction)
+        return sign(prediction)
 
     def partial_fit(self, xs, ys):
         """
-
+        Runs a partial fit of the model on the data
+        1 single run
         :param self:
-        :param xs:
-        :param ys:
+        :param xs: List of instances (data)
+        :param ys: List of target / values
         :return:
         """
         for x, y in zip(xs, ys):
@@ -115,11 +143,12 @@ class Perceptron:
 
     def fit(self, xs, ys, *, epochs=0):
         """
-
+        update/fit the model on the data with either the given amount op epochs,
+        or until there are no more changes in weight and bias.
         :param self:
-        :param xs:
-        :param ys:
-        :param epochs:
+        :param xs: List of instances (data)
+        :param ys: List of target / values
+        :param epochs: Int number of runs
         :return:
         """
         prev_weights = self.weights
@@ -142,6 +171,10 @@ class Perceptron:
 
 
 class LinearRegression:
+    """
+    linear regression model same as perceptron class but with a different activation.
+    """
+
     def __init__(self, dim, bias=0, weights=None, learning_rate=0.01):
         self.dim = dim
         self.bias = bias
@@ -155,6 +188,11 @@ class LinearRegression:
             self.weights = weights
 
     def predict(self, xs):
+        """
+        Calculates the prediction of the given list of instance with the stored weights and bias
+        :param xs: a list of instances
+        :return: list of predictions
+        """
         predictions = []
         for instance in xs:
             prediction = self.predict_instance(instance)
@@ -162,12 +200,26 @@ class LinearRegression:
         return predictions
 
     def predict_instance(self, instance):
+
+        """
+        predicts the given instance with the stored weights and bias
+        :param instance: a single instance array
+        :return: the predicted value int
+        """
         prediction = self.bias
         for i in range(len(instance)):
             prediction += self.weights[i] * instance[i]
-        return prediction
+        return sign(prediction)
 
     def partial_fit(self, xs, ys):
+        """
+                Runs a partial fit of the model on the data
+                1 single run
+                :param self:
+                :param xs: List of instances (data)
+                :param ys: List of target / values
+                :return:
+                """
         for x, y in zip(xs, ys):
             prediction = self.predict_instance(x)
             self.bias -= self.learning_rate * (prediction - y)
@@ -205,21 +257,21 @@ class Neuron:
     def predict(self, xs):
         predictions = []
         for instance in xs:
-            prediction = self.predict_instance(instance)
-            predictions.append(prediction)
+            prediction = self.bias
+            for i in range(len(instance)):
+                prediction += self.weights[i] * instance[i]
+            predictions.append(self.activation(prediction))
         return predictions
-
-    def predict_instance(self, instance):
-        prediction = self.bias
-        for i in range(len(instance)):
-            prediction += self.weights[i] * instance[i]
-        return self.activation(prediction)
 
     def partial_fit(self, xs, ys, alpha=0.001):
         for x, y in zip(xs, ys):
-            prediction = self.predict_instance(x)
+            prediction = self.bias
+            for i in range(len(x)):
+                prediction += self.weights[i] * x[i]
+
             loss_gradient = derivative(self.loss)(prediction, y)
-            activation_gradient = derivative(self.activation)(self.bias + sum(w * xi for w, xi in zip(self.weights, x)))
+            activation_gradient = derivative(self.activation)(prediction)
+
             self.bias -= alpha * loss_gradient * activation_gradient
             for i in range(len(x)):
                 self.weights[i] -= alpha * loss_gradient * activation_gradient * x[i]
@@ -237,7 +289,7 @@ class Neuron:
             prev_bias = self.bias
             epoch += 1
             if epochs != 0 and epoch >= epochs:
-                break  # stop als het opgeven max. aantal epochs behaald is
+                break  # Stop als het opgeven max. aantal epochs behaald is
 
     def __repr__(self):
         text = f'Neuron(dim={self.dim}, activation={self.activation.__name__}, loss={self.loss.__name__})'
